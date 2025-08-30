135d2a611b4e65424700426da772cc44ab119dfa
= <darsh.astro@outlook.com>
2025-08-31 01:44:17 +0530

    README Files Edited and Added base files

---
+++ Neural Networks/ANNs and Perceptrons/PerceptronImplentation.py
@@ -0,0 +1,8 @@
+#Implementing a single perceptron architecture using math functions
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+import seaborn as sns 
+
+
