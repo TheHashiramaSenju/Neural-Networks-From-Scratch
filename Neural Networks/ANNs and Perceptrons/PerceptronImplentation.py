1b352068e950991792a8f27a3cf4edb04c09a6e3
TheHashiramaSenju <darsh.astro@outlook.com>
2025-11-03 14:10:32 +0530

    alll done

--- Neural Networks/ANNs and Perceptrons/PerceptronImplentation.py
+++ Neural Networks/ANNs and Perceptrons/PerceptronImplentation.py
@@ -1,8 +1,6 @@
 #Implementing a single perceptron architecture using math functions
 
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
-import seaborn as sns 
-
-
+class Perceptron:
+    def __init__(self, learning_rate=0.01, n_iters=1000):
+        pass
